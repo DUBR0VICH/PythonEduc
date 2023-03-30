4fd333a5541b6abea5bbea126a8a9fb00f40357c
DUBR0VICH <andrey.dubrov.04@mail.ru>
2023-03-30 10:27:45 +0300

    Update Python-2.py

--- PythonTasks/Python-2.py
+++ PythonTasks/Python-2.py
@@ -2,11 +2,11 @@ string = input("Введите строку: ")
 length = len(string)
 
 for i in range(length):
-    if i == 2:  # Пропускаем 3-й символ
+    if i == 2:
         continue
-    elif string[i] == "c":  # Если есть символ "c", выводим сообщение
+    elif string[i] == "c":
         print("В строке есть символ 'c'!")
-    print(string[i], end="")  # Выводим символ без перевода строки
+    print(string[i], end="")
 
 print("\nДлина строки:", length)
 print("Строка без последнего символа:", string[:-1])
