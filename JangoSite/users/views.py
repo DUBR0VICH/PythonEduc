09d1dd568ecdcfac5a1f89ceb2621ef12ed5a8a3
DUBR0VICH <andrey.dubrov.04@mail.ru>
2023-04-19 17:22:48 +0300

    Login-Register-Logout

--- JangoSite/users/views.py
+++ JangoSite/users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.views import View
+from users.forms import UserCreationForm
+
+class Register(View):
+    template_name = 'registration/register.html'
+
+    def get(self, request):
+        context = {'form': UserCreationForm()}
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        form = UserCreationForm()
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+        context = {'form': form}
+        return render(request, self.template_name, context)
